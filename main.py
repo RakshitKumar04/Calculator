16880ced3df15a2c4d1ac3f758f1deeb97caa563
Julien Perce <58319391+julienperce@users.noreply.github.com>
2020-10-14 09:55:43 -0500

    add division functionality
    add division function, with error if denominator is 0.

--- main.py
+++ main.py
@@ -4,6 +4,13 @@
 def add(num1, num2):
     return num1 + num2
 
+def divide(num1, num2):
+    if num2 == 0:
+        print("That is an invalid division, since division by 0 is not mathematically possible!")
+        return "Unavailable"
+    else:
+        return num1 / num2
+
 def print_welcome_msg():
     print(f'\n\n* Hi, welcome to my program hope you have a good time\n\n')  # Press Ctrl+F8 to toggle the breakpoint.
 
@@ -14,3 +21,4 @@ if __name__ == '__main__':
     a = int(input("please input 1st number: "))
     b = int(input("please input 2nd number: "))
     print(f"\nResult of addition is: {add(a, b)}\n")
+    print(f"\nResult of division is: {divide(a, b)}\n")
