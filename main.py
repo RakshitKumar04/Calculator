335c22b56dcc3c977efc536f132e2cfc37f29373
gupta.divya <gupta.divya@salesforce.com>
2020-10-14 11:11:54 +0530

    Add Function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def add(num1, num2):
+    return num1 + num2
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
