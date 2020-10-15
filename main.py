e785638be8a3bc566d70a4e000a405c3a98cc124
Riya <riyad17031998@gmail.com>
2020-10-15 12:26:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 def add(num1, num2):
     return num1 + num2
 
+def square(num):
+    return num*num
+
 def print_welcome_msg():
     print(f'\n\n* Hi, welcome to my program hope you have a good time\n\n')  # Press Ctrl+F8 to toggle the breakpoint.
 
@@ -14,3 +17,5 @@ if __name__ == '__main__':
     a = int(input("please input 1st number: "))
     b = int(input("please input 2nd number: "))
     print(f"\nResult of addition is: {add(a, b)}\n")
+    print(f"\nResult of square of 1st number is: {square(a)}\n")
+    print(f"\nResult of square of 2st number is: {square(b)}\n")
